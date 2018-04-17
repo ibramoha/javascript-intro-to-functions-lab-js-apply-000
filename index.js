43399ff93beb19c50971d9b52de2eba7cadab284
Ibrahim Mohammed <ibramohauk@gmail.com>
2018-04-17 16:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase()){
-    return "I love you, Grandma."
+    return "I can't hear you!"
   }
   
   else if(string === string.toUpperCase()){
